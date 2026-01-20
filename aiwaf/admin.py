194512b2043636ff269c4a0e68dade0cfaf71d31
Chris Curvey <chris@chriscurvey.com>
2026-01-20 14:53:34 -0500

    add GeoBlockedCountryAdmin

--- aiwaf/admin.py
+++ aiwaf/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
 
-from aiwaf.models import IPExemption, BlacklistEntry
+from aiwaf.models import IPExemption, BlacklistEntry, GeoBlockedCountry
 
 
 @admin.register(BlacklistEntry)
-class BlacklistEntry(admin.ModelAdmin):
+class BlacklistEntryAdmin(admin.ModelAdmin):
     list_display = ('ip_address', 'reason', 'created_at')
 
 
 @admin.register(IPExemption)
-class IPExemption(admin.ModelAdmin):
+class IPExemptionAdmin(admin.ModelAdmin):
     list_display = ('ip_address', 'reason', 'created_at')
+
+
+@admin.register(GeoBlockedCountry)
+class GeoBlockedCountryAdmin(admin.ModelAdmin):
+    list_display = ('country_code', 'reason', 'created_at')
