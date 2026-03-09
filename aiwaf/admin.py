54a6d0c218662e2c182ad186f31d5375dd0910a3
Chris Curvey <chris@chriscurvey.com>
2026-03-09 08:14:24 -0400

    correct admin config

--- aiwaf/admin.py
+++ aiwaf/admin.py
@@ -6,14 +6,14 @@ from aiwaf.models import IPExemption, BlacklistEntry, GeoBlockedCountry
 @admin.register(BlacklistEntry)
 class BlacklistEntryAdmin(admin.ModelAdmin):
     list_display = ('ip_address', 'reason', 'created_at')
-    search_fields = 'ip_address'
+    search_fields = ('ip_address',)
     date_hierarchy = 'created_at'
 
 
 @admin.register(IPExemption)
 class IPExemptionAdmin(admin.ModelAdmin):
     list_display = ('ip_address', 'reason', 'created_at')
-    search_fields = 'ip_address'
+    search_fields = ('ip_address',)
     date_hierarchy = 'created_at'
 
 
